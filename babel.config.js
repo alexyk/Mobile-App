96b9725065667320c55b665cb8625ab7d7505040
alexyk <alexander.kostadinov@locktrip.com>
2019-08-30 19:29:15 +0300

    Added babel-throw-expressions for testing

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function(api) {
   api && api.cache(false);
-    return {
-    "presets": ["module:metro-react-native-babel-preset"],
-    "plugins": [
-      ["@babel/plugin-proposal-decorators", {"legacy": true}],
-      ["@babel/plugin-transform-runtime", {"regenerator": false}]
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-transform-runtime", { regenerator: false }],
+      ["@babel/plugin-proposal-throw-expressions"]
     ]
-  }
-}
+  };
+};
