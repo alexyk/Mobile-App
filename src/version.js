86605223176d9229266943764a58e27b794a2013
alexyk <alexander.kostadinov@locktrip.com>
2019-10-08 18:42:04 +0300

    Release v1.0.25-qa1

--- src/version.js
+++ src/version.js
@@ -6,7 +6,7 @@ const {
   step3BookingDetails
 } = hotelSearchIsNative;
 
-const compilationTime = '2019-09-17 12:28:51 EEST';
+const compilationTime = '2019-10-08 18:27:32 EEST';
 const branchName = ""; // this line is updated by scripts/select_config.rb and travis-version.rb
 const travisVersion = ""; // this line is updated by scripts/select_config.rb and travis-version.rb
 const packJsonVersion = require("../package.json").version;
